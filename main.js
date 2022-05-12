520aada26a3b226df777379cc908a8718d035661
AdityaAdapa <aditya.adapa18@outlook.com>
2022-05-12 08:36:52 -0500

    sixth

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 question = ["3 + (4 * 7) / 2", "55 + 44 / 11 + (7 * 3)"]
 answers = ["17", "80"]
 for (var x; x in question;) {
-    $("#questions").html = "<h1>" + x + "</h1>"
+    document.getElementById("questions").innerHTML += "<h1>" + x + "</h1>"
 } 
